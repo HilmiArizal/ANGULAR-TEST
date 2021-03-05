3c3258edc03cf38ca16fba24b8d211e1754b3ac6
hilmiarizal <hilmi.arizal36@gmail.com>
2021-03-05 19:23:32 +0700

    navigation-material-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MaterialDesign } from './material-ui/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MaterialDesign
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-ui/material.ts
@@ -0,0 +1,12 @@
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+
+export const MaterialDesign = [
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
+]
